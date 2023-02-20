2d95c60ff30464911cfe5f05585adebb151b51ce
OdionAkhes <akhetuamhenodion@gmail.com>
2023-02-20 15:22:23 +0100

    first commit

---
+++ src/components/FilterButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const FilterButton = ({ isPressed ,name, setFilter }) => {
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn toggle-btn"
+        aria-pressed={isPressed}
+        onClick={() => setFilter(name)}
+      >
+        <span className="visually-hidden">Show </span>
+        <span>{name}</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+    </div>
+  );
+};
+
+export default FilterButton
---
+++ src/components/Todo.js
@@ -0,0 +1,121 @@
+/** @format */
+
+import React, { useEffect, useRef, useState } from "react";
+
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+
+
+const Todo = ({ name, id, completed, toggleTaskCompleted, deleteTask , editTask}) =>
+{
+  const [newName, setNewName] = useState("");
+
+  const [isEditing, setEditing] = useState(false);
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+const wasEditing = usePrevious(isEditing);
+
+useEffect(() => {
+  if (!wasEditing && isEditing) {
+    editFieldRef.current.focus();
+  }
+  if (wasEditing && !isEditing) {
+    editButtonRef.current.focus();
+  }
+}, [wasEditing, isEditing]);
+
+
+
+
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    editTask(id, newName);
+    setNewName("");
+    setEditing(false);
+}
+  
+  
+const editingTemplate = (
+  <form className="stack-small" onSubmit={handleSubmit}>
+    <div className="form-group">
+      <label className="todo-label" htmlFor={id}>
+        New name for {name}
+      </label>
+      <input
+        id={id}
+        className="todo-text"
+        type="text"
+        value={newName}
+        onChange={handleChange}
+        ref={editFieldRef}
+      />
+    </div>
+    <div className="btn-group">
+      <button
+        type="button"
+        className="btn todo-cancel"
+        onClick={() => setEditing(false)}
+      >
+        Cancel
+        <span className="visually-hidden">renaming {name}</span>
+      </button>
+
+      <button type="submit" className="btn btn__primary todo-edit">
+        Save
+        <span className="visually-hidden">new name for {name}</span>
+      </button>
+    </div>
+  </form>
+);
+const viewTemplate = (
+  <div className="stack-small">
+    <div className="c-cb">
+      <input
+        id={id}
+        type="checkbox"
+        defaultChecked={completed}
+        onChange={() => toggleTaskCompleted(id)}
+      />
+      <label className="todo-label" htmlFor={id}>
+        {name}
+      </label>
+    </div>
+    <div className="btn-group">
+      <button type="button"
+        className="btn"
+          ref={editButtonRef}
+onClick={() => setEditing(true)}>
+        Edit <span className="visually-hidden">{name}</span>
+      </button>
+
+      <button
+        type="button"
+        className="btn btn__danger"
+        onClick={() => deleteTask(id)}
+      >
+        Delete <span className="visually-hidden">{name}</span>
+      </button>
+    </div>
+  </div>
+);
+
+
+  return (
+    <div>
+
+      <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>
+    </div>
+  );
+};
+
+export default Todo;
